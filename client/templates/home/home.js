f6ddeb86df8373452f80c588ee5732944db2534d
siddharthshukla <s.shukla@jacobs-university.de>
2016-07-02 20:46:04 +0200

    Add signup

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,52 +1,52 @@
-Template.home.onCreated(function() {
-  this.locationVar = new ReactiveVar();
+Template.home.onCreated(function () {
+    this.locationVar = new ReactiveVar();
 });
 
-Template.home.onRendered(function() {
+Template.home.onRendered(function () {
     var platform = new H.service.Platform({
-    'app_id': 'BTyOs9Gi6lXm5zM7zS9P',
-    'app_code': 'QAnZFHMjfbkKXg9aezRngA'
+        'app_id': 'BTyOs9Gi6lXm5zM7zS9P',
+        'app_code': 'QAnZFHMjfbkKXg9aezRngA'
     });
 
     var maptypes = platform.createDefaultLayers();
 
     this.map = new H.Map(
-      document.getElementsByClassName('map-container')[0],
-      maptypes.normal.map,
-      {
-        zoom: 15,
-        center: { lng: 13.4, lat: 52.51 }
-      }
+        document.getElementsByClassName('map-container')[0],
+        maptypes.normal.map,
+        {
+            zoom: 15,
+            center: {lng: 13.4, lat: 52.51}
+        }
     );
-    
+
     this.places = platform.getPlacesService();
 });
 
 Template.home.events({
-  'change input#destination': function(e, t) {
-    //destination unknown known known 
-    t.places.search({
-      q: e.currentTarget.value,
-      in: '53.12,10.15;r=6000000'
-    }, function(r) {
-      if(r.results.items.length > 0) {
-        var res = r.results.items[0];
-        t.map.setCenter({lng: res.position[1], lat: res.position[0]});
-        t.locationVar.set(res);
-      }
-    }, function(e,r) {
-      console.log(e,r);
-    });
-  },
-  'click .page-footer button': function(e, t) {
-    e.preventDefault();
-    console.log(t.locationVar.get());
-    alert('k. it works this is horrible. I pass the torch to someone else');
-  }
+    'change input#destination': function (e, t) {
+        //destination unknown known known
+        t.places.search({
+            q: e.currentTarget.value,
+            in: '53.12,10.15;r=6000000'
+        }, function (r) {
+            if (r.results.items.length > 0) {
+                var res = r.results.items[0];
+                t.map.setCenter({lng: res.position[1], lat: res.position[0]});
+                t.locationVar.set(res);
+            }
+        }, function (e, r) {
+            console.log(e, r);
+        });
+    },
+    'click .page-footer button': function (e, t) {
+        e.preventDefault();
+        Session.set("selfLocationData", t.locationVar.get());
+        Router.go("/signup");
+    }
 });
 
 Template.home.helpers({
-  location: function() {
-    return Template.instance().locationVar.get();
-  }
+    location: function () {
+        return Template.instance().locationVar.get();
+    }
 });
---
+++ client/templates/signup/signup.js
@@ -0,0 +1,16 @@
+Template.signup.events({
+    'submit .signup-form': function(e, t) {
+        e.preventDefault();
+        let data = Template.currentData();
+        console.log(this);
+
+        let text = String(t.$('input[name="message"]').val()).trim();
+        if (text) {
+            Meteor.call('Messages.insert', {
+                rideId: this.ride._id,
+                text: text
+            });
+            t.$('input[name="message"]').val('');
+        }
+    },
+});
