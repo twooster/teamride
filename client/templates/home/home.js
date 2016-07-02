b8ff1c21c5049aad8bba115d1ede97ebc96f22b0
Tony Wooster <twooster@gmail.com>
2016-07-02 14:47:28 +0100

    adds a map

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,3 +1,16 @@
-Template.home.rendered = function() {
+Template.home.onRendered(function() {
+    var platform = new H.service.Platform({
+    'app_id': 'BTyOs9Gi6lXm5zM7zS9P',
+    'app_code': 'QAnZFHMjfbkKXg9aezRngA'
+    });
 
-};
+    var maptypes = platform.createDefaultLayers();
+
+    var map = new H.Map(
+    document.getElementsByClassName('map-container')[0],
+    maptypes.normal.map,
+    {
+      zoom: 10,
+      center: { lng: 13.4, lat: 52.51 }
+    });
+});
