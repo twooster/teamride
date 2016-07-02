445fb98f038b70c4dbeec924c0202afc3e5b32df
Tony Wooster <twooster@gmail.com>
2016-07-02 16:12:39 +0100

    this is someone elses problem now

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,3 +1,7 @@
+Template.home.onCreated(function() {
+  this.locationVar = new ReactiveVar();
+});
+
 Template.home.onRendered(function() {
     var platform = new H.service.Platform({
     'app_id': 'BTyOs9Gi6lXm5zM7zS9P',
@@ -6,11 +10,43 @@ Template.home.onRendered(function() {
 
     var maptypes = platform.createDefaultLayers();
 
-    var map = new H.Map(
-    document.getElementsByClassName('map-container')[0],
-    maptypes.normal.map,
-    {
-      zoom: 10,
-      center: { lng: 13.4, lat: 52.51 }
+    this.map = new H.Map(
+      document.getElementsByClassName('map-container')[0],
+      maptypes.normal.map,
+      {
+        zoom: 15,
+        center: { lng: 13.4, lat: 52.51 }
+      }
+    );
+    
+    this.places = platform.getPlacesService();
+});
+
+Template.home.events({
+  'change input#destination': function(e, t) {
+    //destination unknown known known 
+    t.places.search({
+      q: e.currentTarget.value,
+      in: '53.12,10.15;r=6000000'
+    }, function(r) {
+      if(r.results.items.length > 0) {
+        var res = r.results.items[0];
+        t.map.setCenter({lng: res.position[1], lat: res.position[0]});
+        t.locationVar.set(res);
+      }
+    }, function(e,r) {
+      console.log(e,r);
     });
+  },
+  'click .page-footer button': function(e, t) {
+    e.preventDefault();
+    console.log(t.locationVar.get());
+    alert('k. it works this is horrible. I pass the torch to someone else');
+  }
+});
+
+Template.home.helpers({
+  location: function() {
+    return Template.instance().locationVar.get();
+  }
 });
